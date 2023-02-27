184273c792959efb0d41efdd186e6b1d57786416
Nicolas van Kempen <nvankemp@gmail.com>
2023-02-27 14:01:20 -0500

    Copy blank litterer to get control litterer started

---
+++ src/control/litterer.cpp
@@ -0,0 +1,42 @@
+#include <dlfcn.h>
+#include <unistd.h>
+
+#include <cstdio>
+
+#include <fmt/core.h>
+
+#define EXPORT __attribute__((visibility("default")))
+
+extern "C" EXPORT void* malloc(size_t size) {
+    static decltype(malloc)* nextMalloc = (decltype(malloc)*) dlsym(RTLD_NEXT, "malloc");
+    return nextMalloc(size);
+}
+
+extern "C" EXPORT void free(void* pointer) {
+    static decltype(free)* nextFree = (decltype(free)*) dlsym(RTLD_NEXT, "free");
+    nextFree(pointer);
+}
+
+extern "C" EXPORT void* calloc(size_t n, size_t size) {
+    static decltype(calloc)* nextCalloc = (decltype(calloc)*) dlsym(RTLD_NEXT, "calloc");
+    return nextCalloc(n, size);
+}
+
+extern "C" EXPORT void* realloc(void* pointer, size_t size) {
+    static decltype(realloc)* nextRealloc = (decltype(realloc)*) dlsym(RTLD_NEXT, "realloc");
+    return nextRealloc(pointer, size);
+}
+
+extern "C" EXPORT void* reallocarray(void* pointer, size_t n, size_t size) {
+    static decltype(reallocarray)* nextReallocarray = (decltype(reallocarray)*) dlsym(RTLD_NEXT, "reallocarray");
+    return nextReallocarray(pointer, n, size);
+}
+
+struct Initialization {
+    Initialization() {
+        fmt::print("[LITTERER] Initialized\n");
+        fmt::print("[LITTERER] PID: {}\n", getpid());
+    }
+};
+
+static const Initialization _;
