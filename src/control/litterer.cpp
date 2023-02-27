440bb84d0775163bbe2eaa59b3309eb3f797c4d1
Nicolas van Kempen <nvankemp@gmail.com>
2023-02-27 14:07:29 -0500

    Add reentrant counter

--- src/control/litterer.cpp
+++ src/control/litterer.cpp
@@ -7,29 +7,75 @@
 
 #define EXPORT __attribute__((visibility("default")))
 
+static thread_local uint32_t busy; 
+
 extern "C" EXPORT void* malloc(size_t size) {
     static decltype(malloc)* nextMalloc = (decltype(malloc)*) dlsym(RTLD_NEXT, "malloc");
-    return nextMalloc(size);
+
+    if (busy) {
+        return nextMalloc(size);
+    }
+
+    ++busy;
+    void* allocation = nextMalloc(size);
+    --busy;
+
+    return allocation;
 }
 
 extern "C" EXPORT void free(void* pointer) {
     static decltype(free)* nextFree = (decltype(free)*) dlsym(RTLD_NEXT, "free");
+
+    if (busy) {
+        nextFree(pointer);
+        return;
+    }
+
+    ++busy;
     nextFree(pointer);
+    --busy;
 }
 
 extern "C" EXPORT void* calloc(size_t n, size_t size) {
     static decltype(calloc)* nextCalloc = (decltype(calloc)*) dlsym(RTLD_NEXT, "calloc");
-    return nextCalloc(n, size);
+
+    if (busy) {
+        return nextCalloc(n, size);
+    }
+
+    ++busy;
+    void* allocation = nextCalloc(n, size);
+    --busy;
+
+    return allocation;
 }
 
 extern "C" EXPORT void* realloc(void* pointer, size_t size) {
     static decltype(realloc)* nextRealloc = (decltype(realloc)*) dlsym(RTLD_NEXT, "realloc");
-    return nextRealloc(pointer, size);
+
+    if (busy) {
+        return nextRealloc(pointer, size);
+    }
+
+    ++busy;
+    void* allocation = nextRealloc(pointer, size);
+    --busy;
+
+    return allocation;
 }
 
 extern "C" EXPORT void* reallocarray(void* pointer, size_t n, size_t size) {
     static decltype(reallocarray)* nextReallocarray = (decltype(reallocarray)*) dlsym(RTLD_NEXT, "reallocarray");
-    return nextReallocarray(pointer, n, size);
+
+    if (busy) {
+        return nextReallocarray(pointer, n, size);
+    }
+
+    ++busy;
+    void* allocation = nextReallocarray(pointer, n, size);
+    --busy;
+
+    return allocation;
 }
 
 struct Initialization {
