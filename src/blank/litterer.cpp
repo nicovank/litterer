56ab149fe2a52353be1606e675c3c8e3a684d1e2
Nicolas van Kempen <nvankemp@gmail.com>
2024-01-18 19:47:28 +0000

    Cleanup

--- src/blank/litterer.cpp
+++ src/blank/litterer.cpp
@@ -1,28 +1,37 @@
 #include <dlfcn.h>
+#include <stdlib.h>
 
-#define EXPORT __attribute__((visibility("default")))
-
-extern "C" EXPORT void* malloc(size_t size) {
-    static decltype(malloc)* nextMalloc = (decltype(malloc)*) dlsym(RTLD_NEXT, "malloc");
-    return nextMalloc(size);
+extern "C" void* malloc(size_t size) {
+    static auto* next = reinterpret_cast<decltype(malloc)*>(dlsym(RTLD_NEXT, "malloc"));
+    return next(size);
 }
 
-extern "C" EXPORT void free(void* pointer) {
-    static decltype(free)* nextFree = (decltype(free)*) dlsym(RTLD_NEXT, "free");
-    nextFree(pointer);
+extern "C" void free(void* pointer) {
+    static auto* next = reinterpret_cast<decltype(free)*>(dlsym(RTLD_NEXT, "free"));
+    next(pointer);
 }
 
-extern "C" EXPORT void* calloc(size_t n, size_t size) {
-    static decltype(calloc)* nextCalloc = (decltype(calloc)*) dlsym(RTLD_NEXT, "calloc");
-    return nextCalloc(n, size);
+extern "C" void* calloc(size_t n, size_t size) {
+    static auto* next = reinterpret_cast<decltype(calloc)*>(dlsym(RTLD_NEXT, "calloc"));
+    return next(n, size);
 }
 
-extern "C" EXPORT void* realloc(void* pointer, size_t size) {
-    static decltype(realloc)* nextRealloc = (decltype(realloc)*) dlsym(RTLD_NEXT, "realloc");
-    return nextRealloc(pointer, size);
+extern "C" void* realloc(void* pointer, size_t size) {
+    static auto* next = reinterpret_cast<decltype(realloc)*>(dlsym(RTLD_NEXT, "realloc"));
+    return next(pointer, size);
 }
 
-extern "C" EXPORT void* reallocarray(void* pointer, size_t n, size_t size) {
-    static decltype(reallocarray)* nextReallocarray = (decltype(reallocarray)*) dlsym(RTLD_NEXT, "reallocarray");
-    return nextReallocarray(pointer, n, size);
+extern "C" void* reallocarray(void* pointer, size_t n, size_t size) {
+    static auto* next = reinterpret_cast<decltype(reallocarray)*>(dlsym(RTLD_NEXT, "reallocarray"));
+    return next(pointer, n, size);
+}
+
+extern "C" int posix_memalign(void** memptr, size_t alignment, size_t size) {
+    static auto* next = reinterpret_cast<decltype(posix_memalign)*>(dlsym(RTLD_NEXT, "posix_memalign"));
+    return next(memptr, alignment, size);
+}
+
+extern "C" void* aligned_alloc(size_t alignment, size_t size) {
+    static auto* next = reinterpret_cast<decltype(aligned_alloc)*>(dlsym(RTLD_NEXT, "aligned_alloc"));
+    return next(alignment, size);
 }
