ef5ce28e566ab9cdc79891be0024044de20324c1
Nicolas van Kempen <nvankemp@gmail.com>
2024-02-20 03:13:33 +0000

    Update iterations/footprint

--- src/benchmarks/iterator.cpp
+++ src/benchmarks/iterator.cpp
@@ -73,11 +73,8 @@ int main(int argc, char** argv) {
     std::uint64_t sum = 0;
     for (std::uint64_t i = 0; i < iterations; ++i) {
         const auto offset = std::uniform_int_distribution<std::size_t>(0, allocationSize - 1)(generator);
-
-        for (const auto* object : objects) {
-            const auto* ptr = static_cast<const std::uint8_t*>(object) + offset;
-            sum += *ptr;
-        }
+        const auto* ptr = static_cast<const std::uint8_t*>(objects[i % nObjects]) + offset;
+        sum += *ptr;
     }
     benchmark::DoNotOptimize(sum);
 
