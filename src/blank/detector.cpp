8d971c06ab90628eb581b92739e1bf1e2be9133c
Nicolas van Kempen <nvankemp@gmail.com>
2024-02-28 05:00:28 +0000

    Split interposition code to separate header

--- src/blank/detector.cpp
+++ src/blank/detector.cpp
@@ -1,22 +1,6 @@
-#include <dlfcn.h>
 #include <stdlib.h> // NOLINT(modernize-deprecated-headers)
 
-#ifdef __APPLE__
-#define DYLD_INTERPOSE(_replacment, _replacee)                                                                         \
-    __attribute__((used)) static struct {                                                                              \
-        const void* replacment;                                                                                        \
-        const void* replacee;                                                                                          \
-    } _interpose_##_replacee __attribute__((section("__DATA,__interpose")))                                            \
-    = {(const void*) (unsigned long) &_replacment, (const void*) (unsigned long) &_replacee};
-
-#define GET_REAL_FUNCTION(NAME) ::NAME
-#define INTERPOSE_FUNCTION_NAME(NAME) interpose_##NAME##__
-#define INTERPOSE(NAME) DYLD_INTERPOSE(INTERPOSE_FUNCTION_NAME(NAME), NAME)
-#else
-#define GET_REAL_FUNCTION(NAME) (reinterpret_cast<decltype(::NAME)*>(dlsym(RTLD_NEXT, #NAME)))
-#define INTERPOSE_FUNCTION_NAME(NAME) NAME
-#define INTERPOSE(NAME)
-#endif
+#include <interpose.h>
 
 extern "C" void* INTERPOSE_FUNCTION_NAME(malloc)(size_t size) {
     static auto* real = GET_REAL_FUNCTION(malloc);
---
+++ src/include/interpose.h
@@ -0,0 +1,22 @@
+#ifndef INTERPOSE_H
+#define INTERPOSE_H
+
+#ifdef __APPLE__
+#define DYLD_INTERPOSE(_replacment, _replacee)                                                                         \
+    __attribute__((used)) static struct {                                                                              \
+        const void* replacment;                                                                                        \
+        const void* replacee;                                                                                          \
+    } _interpose_##_replacee __attribute__((section("__DATA,__interpose")))                                            \
+    = {(const void*) (unsigned long) &_replacment, (const void*) (unsigned long) &_replacee};
+
+#define GET_REAL_FUNCTION(NAME) ::NAME
+#define INTERPOSE_FUNCTION_NAME(NAME) interpose_##NAME##__
+#define INTERPOSE(NAME) DYLD_INTERPOSE(INTERPOSE_FUNCTION_NAME(NAME), NAME)
+#else
+#include <dlfcn.h>
+#define GET_REAL_FUNCTION(NAME) (reinterpret_cast<decltype(::NAME)*>(dlsym(RTLD_NEXT, #NAME)))
+#define INTERPOSE_FUNCTION_NAME(NAME) NAME
+#define INTERPOSE(NAME)
+#endif
+
+#endif // INTERPOSE_H
